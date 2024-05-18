d2cb8003ac7e931ce03e9684fcba75e83bc7c6fc
Dante <dadisainer@gmail.com>
2024-05-18 17:58:44 -0600

    component-control-props-alt listo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,26 +9,13 @@ export const useShoppingCart = () => {
 
     setShoppingCart(oldShoppingCart => {
 
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0}
-
-      if(Math.max(productInCart.count + count, 0) > 0){
-        productInCart.count += count
-        return { ...oldShoppingCart, [product.id]:productInCart}
-      }
-      // hay q borrar el producto
-
-      const {[product.id]:toDelete, ...rest} = oldShoppingCart;
-        // console.log(toDelete);
-        return {...rest}
-
-      /*codigo anterior chido
       if(count === 0){
         const {[product.id]:toDelete, ...rest} = oldShoppingCart;
-        console.log(toDelete);
+     //   console.log(toDelete);
         return {...rest}
       }
       return {...oldShoppingCart, [product.id]:{...product, count}
-    }*/
+    }
     })
 }
 
